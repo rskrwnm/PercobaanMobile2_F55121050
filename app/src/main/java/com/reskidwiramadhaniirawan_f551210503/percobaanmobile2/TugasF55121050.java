2b58fef14954d340c40c436f2e9518c675481f2c
rskrwnm <dwireski63@gmail.com>
2022-10-18 22:53:29 +0800

    Initial commit

---
+++ app/src/main/java/com/reskidwiramadhaniirawan_f551210503/percobaanmobile2/TugasF55121050.java
@@ -0,0 +1,14 @@
+package com.reskidwiramadhaniirawan_f551210503.percobaanmobile2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TugasF55121050 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tugas_f55121050);
+    }
+}
